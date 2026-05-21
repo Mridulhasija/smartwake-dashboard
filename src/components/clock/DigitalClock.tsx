91951970e49b46d086d534ae39b3d0dc078bcc1b
Mridul Hasija <mridulhasija11@gmail.com>
2026-05-21 21:54:14 +0530

    Update DigitalClock.tsx

--- src/components/clock/DigitalClock.tsx
+++ src/components/clock/DigitalClock.tsx
@@ -7,7 +7,7 @@ return (
 initial={{ opacity: 0, y: 30 }}
 animate={{ opacity: 1, y: 0 }}
 transition={{ duration: 0.8 }}
-className="bg-gradient-to-r from-cyan-400 via-purple-500 to-pink-500 bgclip-text text-7xl font-black text-transparent md:text-9xl"
+className="bg-gradient-to-r from-cyan-400 via-purple-500 to-pink-500 bg-clip-text text-7xl font-black text-transparent md:text-9xl"
 >
 {time.toLocaleTimeString()}
 </motion.h1>
