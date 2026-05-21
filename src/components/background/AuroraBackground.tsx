a6aca098e1b23ade12c92dc39a1fcef568606f8e
Mridul Hasija <mridulhasija11@gmail.com>
2026-05-21 23:46:48 +0530

    Update AuroraBackground.tsx

--- src/components/background/AuroraBackground.tsx
+++ src/components/background/AuroraBackground.tsx
@@ -1,8 +1,12 @@
 function AuroraBackground() {
 return (
 <div className="absolute inset-0 overflow-hidden">
-<div className="absolute left-0 top-0 h-96 w-96 rounded-full bg-cyan-500/20 blur-3xl" />
-<div className="absolute bottom-0 right-0 h-96 w-96 rounded-full bg-purple-500/20 blur-3xl" />
+<div className="absolute -left-32 top-0 h-[500px] w-[500px] rounded-full
+bg-cyan-500/20 blur-3xl" />
+<div
+className="absolute bottom-0 right-0 h-[500px] w-[500px] rounded-full bgpurple-500/20 blur-3xl" />
+<div
+className="absolute left-1/3 top-1/3 h-[300px] w-[300px] rounded-full bgpink-500/10 blur-3xl" />
 </div>
 )
 }
